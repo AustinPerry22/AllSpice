9409065b64ad1b62759e4cc8c6eaea9da1907958
Austin <austinjperry22@gmail.com>
2023-10-11 17:01:59 -0600

    sql for inserting data into recipies

--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -1,8 +1,40 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS recipes(
+        id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        title VARCHAR(255) NOT NULL,
+        instructions VARCHAR(5000) NOT NULL,
+        img VARCHAR(1000) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8mb4;
+
+INSERT INTO
+    recipes(
+        title,
+        instructions,
+        img,
+        category,
+        creatorId
+    )
+VALUES (
+        'PIZZA',
+        "BUY FROM PIZZA PLACE",
+        "https://images.unsplash.com/photo-1519411792752-25c2468cccb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a3JhZnQlMjBtYWMlMjBuJTIwY2hlZXNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        "PIZZA",
+        "65271adcaf59ea92c77ce897"
+    );
+
+SELECT * FROM recipes;
