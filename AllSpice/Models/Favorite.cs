c94cfd41efee63ce0112100e821bc8c638e1e632
Austin <austinjperry22@gmail.com>
2023-10-12 14:55:18 -0600

    models

---
+++ AllSpice/Models/Favorite.cs
@@ -0,0 +1,8 @@
+namespace AllSpice.Models;
+
+public class Favorite
+{
+    public string Id { get; set; }
+    public string AccountId { get; set; }
+    public int recipeId { get; set; }
+}
---
+++ AllSpice/Models/Ingredient.cs
@@ -0,0 +1,9 @@
+namespace AllSpice.Models;
+
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string quantity { get; set; }
+    public int reciepeId { get; set; }
+}
